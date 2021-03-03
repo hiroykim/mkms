59a9f7b185dba568b2d25d3f61de164630ef41e0
hiroykim <kimjunwon@gmail.com>
2021-03-03 21:53:01 +0900

    first

--- sample/hello_khaiii.py
+++ sample/hello_khaiii.py
@@ -1,3 +1,4 @@
+from khaiii import KhaiiiApi
 # -*- coding: utf-8 -*-
 
 __author__ = "Meritz"
@@ -9,8 +10,17 @@ __date__ = "creation: 2021-03-02, modification: 0000-00-00"
 
 import os
 
+def test():
+    api = KhaiiiApi()
+    for word in api.analyze('아버지가 방에 들어가신다.'):
+        print(word)
+
+
 def main():
     print("Hello Python!")
+    test()
 
 if __name__ == "__main__":
     main()
+
+
