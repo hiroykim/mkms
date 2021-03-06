ab388a55c85d62cd27c2cc6881ad193484c68754
hiroykim <kimjunwon@gmail.com>
2021-03-07 00:34:29 +0900

    2021-03-06

---
+++ start_web1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Meritz"
+__date__ = "creation: 2021-03-06, modification: 0000-00-00"
+
+#################
+#import
+#################
+from web import app
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0',port=8781)
--- web/__init__.py
+++ web/__init__.py
@@ -1,1 +1,12 @@
+from flask import Flask, Response, make_response
 
+app = Flask(__name__)
+
+@app.route('/res1')
+def res1():
+	custom_res = Response("Custrom Response",200,{"test json":"My Value"})
+	return make_response(custom_res)
+
+@app.route("/")
+def helloworld():
+	return "Hello Flask"
